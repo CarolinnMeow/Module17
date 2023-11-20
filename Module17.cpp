89a40c9fe393978553f51a9a86474e65c19281dc
aleksandra_orlova <aleksa.orlik2000@gmail.com>
2023-11-20 15:06:25 +0300

    Добавьте файлы проекта.

---
+++ Module17.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Vector
+{
+private:
+    double x;
+    double y;
+    double z;
+
+public:
+    Vector() : x(0), y(0), z(0)
+    {}
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+
+    void Show()
+    {
+        cout << "Parameters: " << x << ' ' << y << ' ' << z << endl;
+    }
+
+    void CalculateModule()
+    {
+        cout << "Vector length: " << sqrt(x * x + y * y + z * z);
+    }
+};
+
+int main()
+{
+    Vector vec(3, 4, 7);
+    vec.Show();
+    vec.CalculateModule();
+}
+
+
